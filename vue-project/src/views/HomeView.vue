29292e3bd2e7fa8e27f2bc25ab091125ef301b15
ryutaro <footballer0315@gmail.com>
2022-11-12 08:11:34 +0900

    vue-router導入

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <InboxView />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import InboxView from '@/components/InboxView.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    InboxView
+  }
+}
+</script>
