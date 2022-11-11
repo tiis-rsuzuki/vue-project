1a3d44943acc6ecb7b8d21cd34bb6073ae69e709
ryutaro <footballer0315@gmail.com>
2022-11-11 22:56:02 +0900

    vuetify Inbox調整

---
+++ vue-project/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <InboxView />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import InboxView from './components/InboxView.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    InboxView,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
