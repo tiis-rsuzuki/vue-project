e20ff5e3467d647a51564535db0eee2a0d8adb7b
ryutaro <footballer0315@gmail.com>
2022-11-12 17:33:43 +0900

    OrderListにテーブル追加

--- vue-project/src/views/OrderList.vue
+++ vue-project/src/views/OrderList.vue
@@ -4,62 +4,126 @@
         class="py-8 px-6"
         fluid
       >
-      <v-row class="justify-end">
-        <v-col cols="1">
-            <v-btn icon color="secondary" size="x-large" v-on:click="order">
-                <v-icon icon="mdi-plus"></v-icon>
-            </v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-          <v-col
-            v-for="card in cards"
-            :key="card"
-            cols="12"
-          >
-            <v-card>
-                <v-list lines="two">
-                <v-list-subheader>{{ card }}</v-list-subheader>
-                <template v-for="n in 6" :key="n">
-                  <v-list-item>
-                    <template v-slot:prepend>
-                      <v-avatar color="secondary"></v-avatar>
-                    </template>
-
-                    <v-list-item-title>Order {{ n }}</v-list-item-title>
-
-                    <v-list-item-subtitle>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
-                    </v-list-item-subtitle>
-                    <!-- link  -->
-                    <router-link :to="{ path: 'order', query: { id: n }}">View detail</router-link>
-                    <!-- link  -->
-                  </v-list-item>
-
-                  <v-divider
-                    v-if="n !== 6"
-                    :key="`divider-${n}`"
-                    inset
-                  ></v-divider>
-                </template>
-              </v-list>
-            </v-card>
-          </v-col>
-        </v-row>
+      <v-table
+        fixed-header
+        height="500px"
+        hover="true"
+      >
+    <thead>
+      <tr>
+        <th class="text-left">
+          projectNo
+        </th>
+        <th class="text-left">
+          customer
+        </th>
+        <th class="text-left">
+          deadline
+        </th>
+        <th class="text-left">
+          manager
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="order in orders"
+        :key="order.projectNo"
+      >
+        <td>{{ order.projectNo }}</td>
+        <td>{{ order.customer }}</td>
+        <td>{{ order.deadline }}</td>
+        <td>{{ order.manager }}</td>
+      </tr>
+    </tbody>
+  </v-table>
     </v-container>
     </v-main>
   </template>
   
   <script>
-    export default {
-        methods: {
-            order(){
-                this.$router.push('/order')
-            }
-        },
-        data: () => ({
-            cards: ['Recently added orders', 'Deadline is coming up soon'],
-            drawer: null,
-        }),
+  export default {
+    data () {
+      return {
+        orders: [
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+          {
+            projectNo: '202211121700',
+            customer: 'amcd',
+            deadline: '2022/12/24',
+            manager: 'Smith'
+          },
+        ],
+      }
+    },
   }
-  </script>
+</script>
+
+<style scoped>
+
+</style>
